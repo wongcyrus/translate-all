2afde67e6eefe76b07980aa7f296c10ef9d20900
EC2 Default User <ec2-user@ip-172-31-91-232.ec2.internal>
2020-10-15 07:31:21 +0000

    Use AWS::Serverless::StateMachine.

--- src/iterator/app.js
+++ src/iterator/app.js
@@ -1,18 +1,22 @@
-exports.lambdaHandler = async(event, context) => {
-    console.log(JSON.stringify(event));
-    let index = event.iterator.index;
-    let step = event.iterator.step;
-    let count = event.iterator.count;
-    index += step;
+exports.lambdaHandler = async (event, context) => {
+  console.log(JSON.stringify(event));
+  let index = event.iterator.index;
+  let step = event.iterator.step;
+  //TODO: remove temp fix for know bug in SAM https://github.com/awslabs/serverless-application-model/issues/1616
+  let count = +event.iterator.count;
+  index += step;
 
-    console.log(event);
-    if (typeof event.iterator.continue !== 'undefined' && !event.iterator.continue)
-        return event.iterator;
-    else
-        return {
-            index,
-            step,
-            count,
-            continue: index < count
-        };
+  console.log(event);
+  if (
+    typeof event.iterator.continue !== "undefined" &&
+    !event.iterator.continue
+  )
+    return event.iterator;
+  else
+    return {
+      index,
+      step,
+      count: "" + count,
+      continue: index < count,
+    };
 };
